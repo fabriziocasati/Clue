8b7939dd3178fd11d4cff0536618f182012d9753
fabriziocasati <casati.fabrizio@gmail.com>
2016-12-29 14:56:29 +0100

    Window layout test.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,24 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(m_button, SIGNAL (clicked()), this, SLOT (openNewWindow()));
 
+    QWidget *window = new QWidget;
+        QPushButton *button1 = new QPushButton("One");
+        QPushButton *button2 = new QPushButton("Two");
+        QPushButton *button3 = new QPushButton("Three");
+        QPushButton *button4 = new QPushButton("Four");
+        QPushButton *button5 = new QPushButton("Five");
+
+        QHBoxLayout *layout = new QHBoxLayout;
+        layout->addWidget(button1);
+        layout->addWidget(button2);
+        layout->addWidget(button3);
+        layout->addWidget(button4);
+        layout->addWidget(button5);
+
+        window->setLayout(layout);
+        window->show();
+
+
 }
 
 MainWindow::~MainWindow() {
--- window.cpp
+++ window.cpp
@@ -1,10 +1,12 @@
 #include "window.h"
 #include <QPushButton>
+#include <QHBoxLayout>
 
 Window::Window(QWidget *parent) :
     QWidget(parent) {
 // Set size of the window
-    setFixedSize(100, 50);
+    setFixedSize(500, 250);
+    //this->setSizePolicy(f);
 
 // Create and position the button
     m_button = new QPushButton("Hello World", this);
@@ -13,6 +15,23 @@ Window::Window(QWidget *parent) :
 // Create and position the button
     m_button2 = new QPushButton("Ciao", this);
     m_button2->setGeometry(30, 30, 80, 30);
+
+
+        QPushButton *button1 = new QPushButton("1");
+        QPushButton *button2 = new QPushButton("2");
+        QPushButton *button3 = new QPushButton("Three");
+        QPushButton *button4 = new QPushButton("Four");
+        QPushButton *button5 = new QPushButton("Five");
+
+        QHBoxLayout *layout = new QHBoxLayout;
+        layout->addWidget(button1);
+        layout->addWidget(button2);
+        layout->addWidget(button3);
+        layout->addWidget(button4);
+        layout->addWidget(button5);
+
+        this->setLayout(layout);
+
 
 
 }
