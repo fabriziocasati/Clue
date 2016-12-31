253667762ea8afcb06bf1b62eae7f18c56c82df9
fabriziocasati <casati.fabrizio@gmail.com>
2016-12-31 02:18:39 +0100

    Minor changes.

--- player.h
+++ player.h
@@ -1,11 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <iostream>
+
 
 class Player
 {
 public:
     Player();
+
+private:
+    std::string name;
+    int numberOfCards;
+    int weaponCardSet;
+    int suspectCard[9];
+    int roomCard[9];
 };
 
 #endif // PLAYER_H
