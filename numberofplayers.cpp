64528939574e07f18e26a22bc5919da32cd944ef
fabriziocasati <casati.fabrizio@gmail.com>
2017-01-10 21:32:17 +0100

    Update NumberOfPlayers class
    The layout of NumberOfPlayers is updated so that the "Ok" button is
    centered.

--- numberofplayers.cpp
+++ numberofplayers.cpp
@@ -20,15 +20,45 @@ NumberOfPlayers::NumberOfPlayers(QWidget *parent)
 
 QGroupBox *NumberOfPlayers::createNumberOfPlayersGroup()
 {
+    QGroupBox *g = new QGroupBox();
+    QVBoxLayout *vbox = new QVBoxLayout;
+
+    /*
+    vbox->setStretch(0,0);
+    vbox->setStretchFactor(g,0);
+    */
+
+
+
+    g->setLayout(vbox);
+
+
     QGroupBox *groupBox = new QGroupBox("Select the number of players for the new game");
 
     m_button = new QPushButton("Ok", this);
     m_button->setDisabled(true);
     connect(m_button, SIGNAL (clicked()), this, SLOT (openNamesOfThePlayersWindow()));
 
-    QHBoxLayout *vbox = new QHBoxLayout;
+    /*
+    QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+    sizePolicy.setHorizontalStretch(0);
+    sizePolicy.setVerticalStretch(0);
+    sizePolicy.setHeightForWidth(m_button->sizePolicy().hasHeightForWidth());
+    m_button->setSizePolicy(sizePolicy);
+    */
+
+    m_button->setFixedSize(QSize(150,50));
+    vbox->setAlignment(Qt::AlignCenter);
+
+    vbox->setStretch(0,0);
+
+
+
+    //m_button->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+    QHBoxLayout *hbox = new QHBoxLayout;
     //vbox->setAlignment(Qt::AlignLeft);
-    vbox->addWidget(m_button);
+
     //vbox->addStretch(1);
 
 
@@ -38,12 +68,14 @@ QGroupBox *NumberOfPlayers::createNumberOfPlayersGroup()
         numberOfPlayersRadioButton[i] = new QRadioButton(numberQString);
         //numberOfPlayersRadioButton[i] = new QRadioButton("Test");
         connect(numberOfPlayersRadioButton[i], SIGNAL (clicked()), this, SLOT (activateConfirmButton()));
-        vbox->addWidget(numberOfPlayersRadioButton[i]);
+        hbox->addWidget(numberOfPlayersRadioButton[i]);
     }
 
-    groupBox->setLayout(vbox);
+    groupBox->setLayout(hbox);
+    vbox->addWidget(groupBox, 0, Qt::AlignCenter);
+    vbox->addWidget(m_button, 1, Qt::AlignCenter);
 
-    return groupBox;
+    return g;
 }
 
 void NumberOfPlayers::openNamesOfThePlayersWindow()
