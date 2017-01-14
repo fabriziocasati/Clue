0c01401badf1bea5a2d5cf4bf51b8660b1a9bdd7
fabriziocasati <casati.fabrizio@gmail.com>
2017-01-15 00:36:25 +0100

    Create 'CardsOfEachPlayerWindow' class

---
+++ gui/CardsOfEachPlayerWindow.cpp
@@ -0,0 +1,7 @@
+#include "CardsOfEachPlayer.h"
+
+CardsOfEachPlayer::CardsOfEachPlayer()
+{
+
+}
+
---
+++ gui/CardsOfEachPlayerWindow.h
@@ -0,0 +1,13 @@
+#ifndef CARDSOFEACHPLAYER_H
+#define CARDSOFEACHPLAYER_H
+
+#include <QObject>
+#include <QWidget>
+
+class CardsOfEachPlayer
+{
+public:
+    CardsOfEachPlayer();
+};
+
+#endif // CARDSOFEACHPLAYER_H
