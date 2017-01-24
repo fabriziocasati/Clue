bbed3c46fd4204556b658f3818402112f600920c
fabriziocasati <casati.fabrizio@gmail.com>
2017-01-24 22:54:12 +0100

    Add confirmButtonClicked function to NewGameCreationWindow

--- gui/NewGameCreationWindow.h
+++ gui/NewGameCreationWindow.h
@@ -1,11 +1,23 @@
 #ifndef NEWGAMECREATIONWINDOW_H
 #define NEWGAMECREATIONWINDOW_H
 
+#include <QDialog>
 
-class NewGameCreationWindow
+class NewGameCreationWindow : public QDialog
 {
+    Q_OBJECT
+
 public:
     NewGameCreationWindow();
+
+private slots:
+    /*!
+     * \brief Function called when the confirm button of the window is clicked by the user.
+     *
+     * When the user clicks the confirm button of the window, actions related to the window have
+     * to be taken to process the data inserted by the user using the window.
+     */
+    virtual void confirmButtonClicked() = 0;
 };
 
 #endif // NEWGAMECREATIONWINDOW_H
