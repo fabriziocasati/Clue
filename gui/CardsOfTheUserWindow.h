73fe5e4b9ed301fad7a4dc9c05b1c6c8b4d1e965
fabriziocasati <casati.fabrizio@gmail.com>
2017-02-16 21:36:23 +0100

    Refactor CardsOfTheUserWindow.h

--- gui/CardsOfTheUserWindow.h
+++ gui/CardsOfTheUserWindow.h
@@ -1,45 +1,55 @@
 #ifndef CARDSOFTHEUSERWINDOW_H
 #define CARDSOFTHEUSERWINDOW_H
 
-#include <QObject>
-#include <QWidget>
 #include "game/NewGameCreator.h"
-#include <QPushButton>
-#include <QGroupBox>
 #include <QDialog>
+#include <QGroupBox>
+#include <QObject>
+#include <QPushButton>
+#include <QWidget>
 
-
+/*!
+ * \brief Window that the user can use to specify the cards that he owns.
+ *
+ * The user can use this window to specify the cards that he received during the preparation phase
+ * of the game. The user can select the cards from the Room, Suspect and Weapon groups.
+ */
 class CardsOfTheUserWindow : public NewGameCreationWindow
 {
     Q_OBJECT
 
-    public:
-        CardsOfTheUserWindow(NewGameCreator *newGameCreator, QWidget *parent = 0);
+public:
+    /*!
+     * \brief Create the window
+     * \param newGameCreator pointer to the newGameCreator instance
+     * \param parent the parent of the window
+     */
+    CardsOfTheUserWindow(NewGameCreator* newGameCreator, QWidget* parent = 0);
 
-    private:
-        NewGameCreator *newGameCreator;
-        QPushButton *confirmButton;
+private:
+    NewGameCreator* newGameCreator;
+    QPushButton* confirmButton;
 
-        QGroupBox* createGroupBoxes();
+    QGroupBox* createGroupBoxes();
 
-        int nonEmptyName[6];
-        int nonEmptyNames = 0;
-        QHash<QLineEdit**, int> hash;
-        QLineEdit *playerNameLineEdit[6];
-        QLabel *playerNameLabel[6];
+    int nonEmptyName[6];
+    int nonEmptyNames = 0;
+    QHash<QLineEdit**, int> hash;
+    QLineEdit* playerNameLineEdit[6];
+    QLabel* playerNameLabel[6];
 
-        QCheckBox *roomCardCheckBox[9];
-        QCheckBox *suspectCardCheckBox[9];
-        QCheckBox *weaponCardCheckBox[9];
+    QCheckBox* roomCardCheckBox[9];
+    QCheckBox* suspectCardCheckBox[9];
+    QCheckBox* weaponCardCheckBox[9];
 
-        int userCardCount;
-        int checkedCheckBoxCount;
+    int userCardCount;
+    int checkedCheckBoxCount;
 
-        QGroupBox* foo(QString string, std::vector<QString> cardList, QCheckBox *cardArray[]);
+    QGroupBox* foo(QString string, std::vector<QString> cardList, QCheckBox* cardArray[]);
 
-    private slots:
-        void confirmButtonClicked();
-        void checkEnablingConditions();
+private slots:
+    void confirmButtonClicked();
+    void checkEnablingConditions();
 };
 
 #endif // CARDSOFTHEUSERWINDOW_H
